3fc02e649a9f20be5b5194c898a9d79663055d48
bskipiala <bartlomiej.skipiala@edu.uekat.pl>
2021-11-07 13:26:41 +0100

    Commit 2

---
+++ zad_1.py
@@ -0,0 +1,7 @@
+names = ["Ala", "Ola", "Kasia", "Basia", "Bartek"]
+
+def getNames(names):
+    for name in names:
+        print(name)
+
+getNames(names)
---
+++ zad_2.py
@@ -0,0 +1,7 @@
+def multiplyNumbers(numbers):
+    for number in numbers:
+        number *= 2
+
+numbers = [1, 2, 3, 4, 5]
+
+multiplyNumbers(numbers)
