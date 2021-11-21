3b59095f7b8d03332893b0478905b4651132f583
Pawel <pawelantolak@interia.eu>
2021-11-21 14:46:39 +0100

    Pierwsze 6 zadań

--- zad_6.py
+++ zad_6.py
@@ -1,7 +1,9 @@
 def fun(list1: list, list2: list) -> list:
     listAll = list1 + list2
-    listAll = set(listAll)
+    set(listAll)
     listPower = []
     for element in listAll:
         listPower.append(element**3)
     return listPower
+
+print(fun([1,2,3], [2, 3, 4, 5]))
