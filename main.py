92570454218c1d5f012d68290e4b21cc33829469
bskipiala <bartlomiej.skipiala@edu.uekat.pl>
2022-01-13 20:55:27 +0100

    person_detection

--- main.py
+++ main.py
@@ -16,16 +16,20 @@ model.setInputScale(1.0/127.5)
 model.setInputMean((127.5, 127.5, 127.5))
 model.setInputSwapRB(True)
 
-img = cv2.imread('example.jpg')
+img = cv2.imread('example1.jpg')
 
-ClassIndex, confidence, bbox = model.detect(img, confThreshold=0.5)
-print(ClassIndex)
+ClassIndex, confidence, bbox = model.detect(img, confThreshold=0.57)
 
 font_scale = 1.5
 font = cv2.FONT_HERSHEY_PLAIN
 for ClassInd, conf, boxes in zip(ClassIndex.flatten(), confidence.flatten(), bbox):
-        cv2.rectangle(img, boxes, (0, 255, 0), 2)
-        cv2.putText(img, classLabels[ClassInd-1], (boxes[0]+10, boxes[1]+40), font, fontScale=font_scale, color=(255, 0, 0), thickness=2)
+    if(ClassInd == 1):
+        cv2.rectangle(img, boxes, (0, 255, 0), 3)
+        cv2.putText(img, classLabels[ClassInd-1], (boxes[0]+10, boxes[1]+40), font, fontScale=font_scale, color=(0, 0, 255), thickness=2)
+    elif (ClassInd == 3):
+        cv2.rectangle(img, boxes, (255, 0, 0), 3)
+        cv2.putText(img, classLabels[ClassInd-1], (boxes[0]+10, boxes[1]+40), font, fontScale=font_scale, color=(0, 0, 255), thickness=2)
+
 
 plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 plt.waitforbuttonpress()
