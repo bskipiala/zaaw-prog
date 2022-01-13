6b61ff567d6df559c6e7011582a2c5359e91ed9c
bskipiala <bartlomiej.skipiala@edu.uekat.pl>
2022-01-13 20:09:10 +0100

    person_detection

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 import cv2
-import numpy as np
-import tensorflow as tf
+import matplotlib.pyplot as plt
 
 config_file = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
 frozen_model = 'frozen_inference_graph.pb'
 
 model = cv2.dnn_DetectionModel(frozen_model, config_file)
 
-label = 'person'
+classLabels = []
+file_name = 'labels.txt'
+with open(file_name, 'rt') as fpt:
+    classLabels = fpt.read().rstrip('\n').split('\n')
+
+model.setInputSize(320, 320)
+model.setInputScale(1.0/127.5)
+model.setInputMean((127.5, 127.5, 127.5))
+model.setInputSwapRB(True)
 
 img = cv2.imread('example.jpg')
 
-plt.imshow(img)
+ClassIndex, confidence, bbox = model.detect(img, confThreshold=0.5)
+print(ClassIndex)
+
+font_scale = 1.5
+font = cv2.FONT_HERSHEY_PLAIN
+for ClassInd, conf, boxes in zip(ClassIndex.flatten(), confidence.flatten(), bbox):
+        cv2.rectangle(img, boxes, (0, 255, 0), 2)
+        cv2.putText(img, classLabels[ClassInd-1], (boxes[0]+10, boxes[1]+40), font, fontScale=font_scale, color=(255, 0, 0), thickness=2)
+
+plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+plt.waitforbuttonpress()
 
